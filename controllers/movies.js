a51eda89308d37f03736d04c4c2db2add0b2bdd8
jhaff <jacobhaff2@gmail.com>
2018-09-20 12:57:28 -0700

    Reviews show below their respective movie.  Added reviews partial and edited movies-show.handlebars.

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const MovieDb = require('moviedb-promise')
 const moviedb = new MovieDb('1506b0c38289216debb158893384fee0')
+const Review = require('../models/review.js');
 
 app.get('/', (req, res) => {
   moviedb.miscNowPlayingMovies().then(response => {
@@ -10,21 +11,23 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies/:id', (req, res) => {
-  moviedb.movieInfo({ id: req.params.id }).then(movie => {
-    if (1) {
-      moviedb.movieVideos({ id: req.params.id }).then(videos => {
-        movie.trailer_youtube_id = videos.results[0].key
-        renderTemplate(movie)
+          moviedb.movieInfo({ id: req.params.id }).then((movie) => {
+              Review.find({movieId: req.params.id}).then((reviews) =>{
+  //                console.log(reviews);
+                  if (movie.video) {
+                      moviedb.movieVideos({ id: req.params.id }).then(videos => {
+                          movie.trailer_youtube_id = videos.results[0].key;
+                          renderTemplate(movie, reviews);
+                      })
+                  } else {
+                      renderTemplate(movie, reviews);
+                  }
+
+                  function renderTemplate(movie, reviews)  {
+                      res.render('movies-show', { movie: movie , reviews:reviews});
+                  }
+              })
+          }).catch(console.error)
       })
-    } else {
-      renderTemplate(movie)
-    }
-
-    function renderTemplate(movie)  {
-      res.render('movies-show', { movie: movie });
-    }
-
-  }).catch(console.error)
-})
 
 module.exports = app;
